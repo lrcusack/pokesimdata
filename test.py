837d595b8f1900233c672252d26503b8ad011c50
Liam Cusack <lrcusack7@gmail.com>
2017-03-28 22:28:51 -0400

    cleaned up test.py to include unittest test cases, failing on some battle/stat mechanics, test values might be wrong

--- test.py
+++ test.py
@@ -1,247 +1,245 @@
-from pokedatasim import Pokemon
-from pokedatasim import Trainer
-from pokedatasim import BigFullFactorial
-from pokedatasim import PokeDataSimulation
+from pokedatasim import *
 import pandas as pd
 import numpy as np
 import sqlite3
+import unittest
+
+
+class TestPokemonClass(unittest.TestCase):
+    """This class tests the Pokemon class for basic functionality"""
+    def checkPokemon(self, pokemonobjects, pokemonnames):
+        if not isinstance(pokemonobjects, list):
+            pokemonobjects = [pokemonobjects]
+        if not isinstance(pokemonnames,list):
+            pokemonnames = [pokemonnames]
+        for idx, p in enumerate(pokemonobjects):
+            with self.subTest(i=idx):
+                self.assertIsInstance(p, Pokemon)
+                self.assertEqual(p.name, pokemonnames[idx])
+
+    def test_constructor(self):
+        name = "Bulbasaur"
+        type1 = "Grass"
+        type2 = "Poison"
+        hp = 45 # should become 149
+        attack = 49 # should become 98
+        defense = 49 # should become 98
+        spatk = 65 # should become 114
+        spdef = 65 # should become 114
+        speed = 45 # should become 94
+        bulba = Pokemon(name, type1, type2, hp, attack, defense, spatk, spdef, speed)
+        self.checkPokemon(bulba, "Bulbasaur")
+        self.assertEqual(bulba.type, ["Grass", "Poison"])
+        self.assertEqual(bulba.hp, 149)
+        self.assertEqual(bulba.attack, 98)
+        self.assertEqual(bulba.defense, 98)
+        self.assertEqual(bulba.spatk, 114)
+        self.assertEqual(bulba.spdef, 114)
+        self.assertEqual(bulba.speed, 94)
+
+    def test_dynamic_creation(self):
+        # setup test variables
+        db = sqlite3.connect('pokedex.sqlite')
+        poketable = pd.read_sql_query("SELECT * FROM Pokemon", db, index_col="index")
+        pikachuindex = 30  # pikachu
+        vcbIndices = [2, 6, 11]  # Venusaur, Charizard, Blastoise
+        checklist = ['Venusaur', 'Charizard', 'Blastoise']
+        fullpokegen = Pokemon.createPokemonGenerator(poketable)
+        cutpokegen = Pokemon.createPokemonGenerator(poketable.iloc[vcbIndices + [pikachuindex]])
+
+        # test single row creation from Data frame
+        pikachu = Pokemon.fromDataFrame(poketable.iloc[pikachuindex])
+        self.checkPokemon(pikachu, "Pikachu")
+
+        # test multi row creation from data frame
+        vcb = Pokemon.fromDataFrame(poketable.iloc[vcbIndices])
+        self.assertIsInstance(vcb, list)
+        self.checkPokemon(vcb, checklist)
+
+        # test single row pokegen with full df
+        pikachufullgen = fullpokegen(pikachuindex)
+        self.checkPokemon(pikachufullgen, "Pikachu")
+
+        # test multi row pokegen with full df
+        vcbfullgen = fullpokegen(vcbIndices)
+        self.assertIsInstance(vcbfullgen, list)
+        self.checkPokemon(vcbfullgen, checklist)
+
+        # test single row pokegen with cut df
+        pikachucutgen = cutpokegen(pikachuindex)
+        self.checkPokemon(pikachucutgen, "Pikachu")
+
+        # test multi row pokegen with cut df
+        vcbcutgen = cutpokegen(vcbIndices)
+        self.assertIsInstance(vcbcutgen, list)
+        self.checkPokemon(vcbcutgen, checklist)
+
+    def test_battlemechanics(self):
+        b = Pokemon("Bulbasaur", "Grass", "Poison", 45, 49, 49, 65, 65, 45)
+        inferiorb = Pokemon("Bulbasaur", "Grass", "Poison", 44, 48, 48, 64, 64, 44)
+        c = Pokemon("Charmander", "Fire", "", 39, 52, 43, 60, 50, 65)
+        s = Pokemon('Squirtle', 'Water', '', 44, 48, 65, 50, 64, 43)
+
+        # test damage
+        orighp = b.hp
+        damage = 15
+        b.takeDamage(damage)
+        self.assertEqual(b.hp, orighp - damage)
+
+        # test isKO
+        b.hp = 0
+        self.assertTrue(b.isKO())
+
+        # test recover
+        b.recover()
+        self.assertEqual(b.hp, b.maxhp)
+
+        # test inferior
+        self.assertTrue(inferiorb.deterministicallyInferiorTo(b))
+
+        # test damage (numbers from azure heights lab battle damage calculator
+        # bulbasaur attacks charmander 22
+        self.assertEqual(b.doAttack(c), 22)
+        # bulbasaur attacks squirtle 41
+        self.assertEqual(b.doAttack(s), 41)
+        # charmander attacks bulbasaur 39
+        self.assertEqual(c.doAttack(b), 39)
+
+        b.recover()
+        s.recover()
+        c.recover()
+
+        # charmander attacks squirtle 11
+        self.assertEqual(c.doAttack(s), 11)
+        # squirtle attacks bulbasaur 14
+        self.assertEqual(s.doAttack(b), 14)
+        # squirtle attacks charmander 15
+        self.assertEqual(s.doAttack(c), 15)
+
+
+class TestTrainerClass(unittest.TestCase):
+    """This class tests the Trainer class for basic functionality"""
+    def test_constructor(self):
+        b = Pokemon("Bulbasaur", "Grass", "Poison", 45, 49, 49, 65, 65, 45)
+        c = Pokemon("Charmander", "Fire", "", 39, 52, 43, 60, 50, 65)
+        s = Pokemon('Squirtle', 'Water', '', 44, 48, 65, 50, 64, 43)
+        t = Trainer([b, c, s])
+
+        self.assertIsInstance(t.pokemon, list)
+        self.assertIsInstance(t.pokemon[0], Pokemon)
+
+    def test_selectionmechanics(self):
+        b = Pokemon("Bulbasaur", "Grass", "Poison", 45, 49, 49, 65, 65, 45)
+        c = Pokemon("Charmander", "Fire", "", 39, 52, 43, 60, 50, 65)
+        s = Pokemon('Squirtle', 'Water', '', 44, 48, 65, 50, 64, 43)
+
+        # test select next conscious pokemon
+        t = Trainer([b, c, s])
+        t.pokemon[0].hp = 0 # knock out first pokemon
+        t.chooseNextPokemon()
+        self.assertEqual(t.activePokemonIdx, 1)
+
+        # test activePokemon
+        self.assertEqual(t.activePokemon().name,"Charmander")
+
+        # test that -1 is returned if all pokemon are KOed
+        for p in t.pokemon:
+            p.hp = 0
+        t.chooseNextPokemon()
+        self.assertEqual(t.activePokemonIdx,-1)
+
+        # test reset
+        t.reset()
+        self.assertEqual(t.activePokemonIdx, 0)
+        self.assertEqual(t.activePokemon().hp, t.activePokemon().maxhp)
+
+    def test_battlemechanics(self):
+        bs = []
+        cs = []
+        ss = []
+        for i in range(6):
+            bs.append(Pokemon("Bulbasaur", "Grass", "Poison", 45, 49, 49, 65, 65, 45))
+            cs.append(Pokemon("Charmander", "Fire", "", 39, 52, 43, 60, 50, 65))
+            ss.append(Pokemon('Squirtle', 'Water', '', 44, 48, 65, 50, 64, 43))
+        btrainer = Trainer(bs)
+        ctrainer = Trainer(cs)
+        strainer = Trainer(ss)
+
+        # test correct order, whoever has higher speed will attack first and KO opponent
+        ctrainer.activePokemon().hp = 1
+        btrainer.activePokemon().hp = 1
+        ctrainer.takeTurn(btrainer)
+        self.assertEqual(btrainer.activePokemonIdx,1)
+        self.assertEqual(ctrainer.activePokemonIdx,0)
+
+        # test type effectiveness (sanity check)
+        ctrainer.reset()
+        btrainer.reset()
+        self.assertTrue(btrainer.fight(strainer))
+        btrainer.reset()
+        strainer.reset()
+        self.assertTrue(ctrainer.fight(btrainer))
+        btrainer.reset()
+        ctrainer.reset()
+        self.assertTrue(strainer.fight(ctrainer))
+
+
+class TestWorkerFunctions(unittest.TestCase):
+    """This class tests the Big Factorial Class"""
+    samplelevels = [[4, 2, 3],[2, 2],[3, 3, 3],[2, 5, 4]]
+
+    @staticmethod
+    def standardFullFact(levels):
+        nfact = len(levels)
+        ncases = np.prod(levels)
+        casematrix = np.zeros((ncases, nfact))
+
+        lev_repeat = 1
+        pattern_repeat = ncases
+        for fact in range(nfact):
+            pattern_repeat //= levels[fact]
+            level = []
+            for val in range(levels[fact]):
+                level += [val] * lev_repeat
+            pattern = level * pattern_repeat
+            lev_repeat *= levels[fact]
+            casematrix[:, fact] = pattern
+
+        return [list(casematrix[i, :]) for i in range(ncases)]
+
+    def test_BigFullFactClass(self):
+        for levels in self.samplelevels:
+            with self.subTest(i=levels):
+                bff = BigFullFactorial(levels)
+                bff_cases = [bff.getCaseFromIndex(i) for i in bff.idxrange]
+                sff_cases = self.standardFullFact(levels)
+
+                # check that standard full factorial and big full factorial are identical
+                for bffc in bff_cases:
+                    self.assertTrue(bffc in sff_cases)
+                    sff_cases.remove(bffc)
+
+                # check that getIndexFromCase and getCaseFromIndex are inverse
+                for idx in bff.idxrange:
+                    self.assertEqual(bff.getIndexFromCase(bff.getCaseFromIndex(idx)), idx)
+
+    def test_db_loading(self):
+        # test load poketable
+        poketable = load_pokemon_table_from_db()
+        self.assertEqual(list(poketable), ['num', 'name', 'type1', 'type2', 'total', 'hp', 'attack', 'defense',
+                                          'spatk', 'spdef', 'speed', 'generation', 'legendary'])
+        self.assertEqual(len(poketable), 800)
+
+        # test load type modifier table
+        typemodifiertable = load_type_modifier_table_from_db()
+        types = list(typemodifiertable)
+        self.assertEqual(types, list(typemodifiertable.index))
+
+if __name__ == "__main__":
+    unittest.main()
+
 
 """
-# test typeModifierTable load
-typeModifierTable = pd.read_table("type_modifiers.csv", index_col = 0,sep=",")
-
-print (typeModifierTable)
-"""
-
-"""
-# test random number generator
-for i in range(100) :
-    print(str(int(217 + (255-217)*np.random.rand())))
-"""
-
-"""
-# test pokemon construction and interaction
-def printHitPoints(pokemon) :
-    print(pokemon.name + " has " + str(pokemon.hp) + " hit points")
-
-bulba = Pokemon("Bulbasaur","Grass","Poison",45,49,49,65,65,45)
-bulba2 = bulba
-print(bulba.name)
-
-charm = Pokemon("Charmander","Fire","",39,52,43,60,50,65)
-print (charm.name)
-
-printHitPoints(charm)
-bulba.doAttack(charm)
-printHitPoints(charm)
-
-printHitPoints(bulba)
-charm.doAttack(bulba)
-printHitPoints(bulba)
-
-printHitPoints(bulba2)
-"""
-
-"""
-# Test trainer construction and interaction, plus reset
-b1 = Pokemon("Bulbasaur1","Grass","Poison",45,49,49,65,65,45)
-b2 = Pokemon("Bulbasaur2","Grass","Poison",45,49,49,65,65,45)
-b3 = Pokemon("Bulbasaur3","Grass","Poison",45,49,49,65,65,45)
-b4 = Pokemon("Bulbasaur4","Grass","Poison",45,49,49,65,65,45)
-b5 = Pokemon("Bulbasaur5","Grass","Poison",45,49,49,65,65,45)
-b6 = Pokemon("Bulbasaur6","Grass","Poison",45,49,49,65,65,45)
-
-c1 = Pokemon("Charmander1","Fire","",39,52,43,60,50,65)
-c2 = Pokemon("Charmander2","Fire","",39,52,43,60,50,65)
-c3 = Pokemon("Charmander3","Fire","",39,52,43,60,50,65)
-c4 = Pokemon("Charmander4","Fire","",39,52,43,60,50,65)
-c5 = Pokemon("Charmander5","Fire","",39,52,43,60,50,65)
-c6 = Pokemon("Charmander6","Fire","",39,52,43,60,50,65)
-
-s1 = Pokemon('Squirtle1','Water','',44,48,65,50,64,43)
-s2 = Pokemon('Squirtle2','Water','',44,48,65,50,64,43)
-s3 = Pokemon('Squirtle3','Water','',44,48,65,50,64,43)
-s4 = Pokemon('Squirtle4','Water','',44,48,65,50,64,43)
-s5 = Pokemon('Squirtle5','Water','',44,48,65,50,64,43)
-s6 = Pokemon('Squirtle6','Water','',44,48,65,50,64,43)
-
-btrainer = Trainer(b1,b2,b3,b4,b5,b6)
-strainer = Trainer(s1,s2,s3,s4,s5,s6)
-ctrainer = Trainer(c1,c2,c3,c4,c5,c6)
-
-# bulbasaur vs charmander
-if btrainer.fight(ctrainer) :
-    print ("b won")
-else :
-    print ("c won")
-
-# squirtle vs veteran charmander
-if strainer.fight(ctrainer) :
-    print ("s won")
-else :
-    print ("c won")
-
-# defeated bulbasaur vs veteran squirtle
-if btrainer.fight(strainer) :
-    print ("b won")
-else :
-    print ("s won")
-
-#reset bulbasaur vs veteran squirtle
-btrainer.reset()
-if btrainer.fight(strainer) :
-    print ("b won")
-else :
-    print ("s won")
-"""
-
-"""
-# test poketable load + identify dominant pokemon
-poketable = pd.read_table("Pokemon.csv",sep=";")
-poketable.index = range(len(poketable.index)) # ensure index starts at 0
-print(poketable)
-
-pokemon = []
-for i in range(len(poketable.index)) :
-    name = poketable.name[i]
-    type1 = poketable.type1[i]
-    type2 = poketable.type2[i]
-    hp = poketable.hp[i]
-    attack = poketable.attack[i]
-    defense = poketable.defense[i]
-    spatk = poketable.spatk[i]
-    spdef = poketable.spdef[i]
-    speed = poketable.speed[i]
-    pokemon.append(Pokemon(name,type1,type2,hp,attack,defense,spatk,spdef,speed))
-print(len(pokemon))
-
-dominantpokemon = []
-for p1 in pokemon :
-    dominant = True
-    for p2 in pokemon:
-        if p1.deterministicallyInferior(p2) :
-            dominant = False
-            break
-    if dominant :
-        dominantpokemon.append(p1)
-print(len(dominantpokemon))
-
-gen1dominant = []
-for p in dominantpokemon:
-    if p.id < 168:
-        print(p.name)
-        gen1dominant.append(p)
-print (len(gen1dominant))
-
-x = (800 ** 12) + 1
-print(x)
-"""
-
-"""
-# test sqlite, adding poketable and type modifier table to database and reading from it
-db = sqlite3.connect("pokedex.sqlite")
-c = db.cursor()
-
-tmt_orig = pd.read_csv("type_modifiers.csv",sep=';', index_col = 0)
-tmt_orig.to_sql("TypeModifier", db)
-
-#poketable_orig = pd.read_table("Pokemon.csv",sep=";")
-#poketable_orig.to_sql("Pokemon", db)
-
-tmtable = pd.read_sql_query("SELECT * FROM TypeModifier", db, index_col="index")
-print(tmtable)
-"""
-
-"""
-# test creating trainers with Pokemon.fromDataFrame and battling
-db = sqlite3.connect("pokedex.sqlite")
-poketable = pd.read_sql_query("SELECT * FROM Pokemon", db, index_col="index")
-
-#print(poketable)
-pokemonRanges1 = range(166,172)
-pokemonRanges2 = range(6,12)
-pokemonRanges2test = [6,7,8,9,10,11]
-
-pokemonIndices1 = [x for x in [2,6,11,168,171,174]]
-pokemonIndices2 = [x for x in [156,157,158,162,165,165]]
-
-print("Pokemon Ranges 1:", str(pokemonRanges1))
-print("Pokemon Ranges 2:", str(pokemonRanges1))
-print("Pokemon Indices 1:", str(pokemonIndices1))
-print("Pokemon Indices 2:", str(pokemonIndices1))
-
-useRanges = False
-
-if useRanges :
-    t1_df = poketable.loc[[x for x in pokemonRanges1]]
-    t2_df = poketable.loc[[x for x in pokemonRanges2]]
-    t2_df_test = poketable.loc[[x for x in pokemonRanges2test]]
-    print("type t2df :",type(t2_df))
-    print("type t2dftest :",type(t2_df_test))
-    print("test equal:",str(t2_df==t2_df_test))
-    print("t2_df type 2:",type(t2_df.type2[9]))
-    print("t2_dftest type 2:",type(t2_df_test.type2[9]))
-else:
-    t1_df = poketable.loc[[x for x in pokemonIndices1]]
-    t2_df = poketable.loc[[x for x in pokemonIndices2]]
-
-print (t1_df)
-print(t2_df)
-
-t1 = Trainer(Pokemon.fromDataFrame(t1_df))
-t2 = Trainer(Pokemon.fromDataFrame(t2_df))
-
-print(t1.pokemon[2].type)
-
-print(t1.fight(t2))
-"""
-
-"""
-#test pokegen
-db = sqlite3.connect("pokedex.sqlite")
-poketable = pd.read_sql_query("SELECT * FROM Pokemon", db, index_col="index")
-pokegen = Pokemon.createPokemonGenerator(poketable)
-pokemonIndices1 = [2,6,11,168,171,174]
-pokemonIndices2 = [156,157,158,162,165,165]
-
-i = 1
-pokemon = Pokemon.fromDataFrame(poketable.loc[i])
-
-pokemon = [Pokemon.fromDataFrame(poketable.loc[x]) for x in pokemonIndices1]
-
-t1 = Trainer(pokegen(pokemonIndices1))
-t2 = Trainer(pokegen(pokemonIndices2))
-
-print([str(p.name) for p in t1.pokemon])
-print([str(p.name) for p in t2.pokemon])
-
-print(t1.fight(t2))
-t1.reset()
-t2.reset()
-print(t2.fight(t1))
-"""
-
-"""
-#test hashing
-x = BigFullFactorial([4,2,3])
-print(x.getCaseFromIndex(1))
-print(x.getCaseFromIndex(8))
-print(x.getCaseFromIndex(23))
-print(x.getIndexFromCase(x.getCaseFromIndex(1)))
-print(x.getIndexFromCase(x.getCaseFromIndex(8)))
-print(x.getIndexFromCase(x.getCaseFromIndex(23)))
-"""
-
-"""
-#testing lambdas
-values = ["a","b","c","c"]
-indices = [5,7,2,2]
-indextuples = enumerate(indices)
-indexlookup = lambda x: [prototypeIdx \
-                                 for prototypeIdx, pokemonIdx in indextuples if pokemonIdx == x].pop()
-print(indexlookup(7))
-"""
-
 pds = PokeDataSimulation("test")
 pds.runSimulation()
 
@@ -257,3 +255,5 @@ for i in resultstable.index:
     middle = int(len(case)/2)
     print([pds.pokegen([pds.pokemonIndices[case[c]]])[0].name for c in case[:middle]],\
           'vs', [pds.pokegen([pds.pokemonIndices[case[c]]])[0].name for c in case[middle:]])
+
+"""
