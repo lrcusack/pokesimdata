e20a923882a0754d8ec889c1d59b6e2df89ca8e4
Liam Cusack <lrcusack7@gmail.com>
2017-04-01 10:42:20 -0400

    added logging class to dynamically log class info and calling function

---
+++ loggable.py
@@ -0,0 +1,15 @@
+import logging
+import inspect
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Loggable:
+    @classmethod
+    def format_msg(cls,msg):
+        return cls.__name__ + ':' + inspect.getouterframes(inspect.currentframe(), 2)[1][3] + ': ' + msg
+
+    @classmethod
+    def dbg(cls, msg='', *args, **kwargs):
+        msg = cls.format_msg(msg)
+        logging.debug(msg, *args, **kwargs)
